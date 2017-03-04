632b1c6830d3e31a2f5c582b953ce5b23e551337
MariaRafailova <mrafailova@gmail.com>
2017-03-04 21:59:37 +0200

    Add files via upload

---
+++ Batteries.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Batteries
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var capacities = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            var usageHours = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            var hours = int.Parse(Console.ReadLine());
+
+            double used = 0;
+            double result = 0;
+            double percent = 0;
+            
+            for (int i = 0; i < capacities.Length; i++)
+            {
+                used = usageHours[i] * hours;
+                result = capacities[i] - used;
+                percent = 100 - ((used*100.0)/ capacities[i]);
+                int index = i + 1;
+                if (result>0)
+                {
+                    Console.WriteLine($"Battery {index}: {result:F2} mAh ({percent:F2})%");
+                }
+                else
+                {
+                    var lasted = Math.Ceiling(capacities[i] / usageHours[i]);
+                    Console.WriteLine($"Battery {index}: dead (lasted {lasted} hours)");                    
+                }           
+               
+            }
+        }
+    }
+}
---
+++ DecodeRadioFrequencies.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DecodeRadioFrequencies
+{
+    public class DecodeRadioFrequencies
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(" .".ToCharArray())
+                .Select(int.Parse)
+                .ToList();
+            
+            var result1 = new List<char>();
+            var result2 = new List<char>();
+           
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (i%2 == 0 && input[i] != 0)
+                {
+                    result1.Add((char)input[i]);                    
+                }
+
+                if (i%2 !=0 && input[i] != 0)
+                {                    
+                    result2.Add((char)input[i]);                    
+                }
+            }
+
+            result2.Reverse();
+            
+            Console.Write(string.Join("", result1));
+            Console.WriteLine(string.Join("", result2));
+        }
+    }
+}
